1508dfde1ec738d86542e7a6e43d8711663772dd
Valentin Dobrica <valentin.dobrica@rei-d-services.com>
2019-02-07 17:25:36 +0200

    REACT GOD

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from './Burger.css';
 import BurgetIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIgredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgetIngredient key={igKey + i} type={igKey}/>
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if(transformedIgredients.length === 0 )
+    {
+        transformedIgredients = <p>Please start add ingredients!</p>
+    }
+    console.log(transformedIgredients);
+
     return (
         <div className={classes.Burger}>
             <BurgetIngredient type="bread-top"/>
-            <BurgetIngredient type="cheese"/>
-            <BurgetIngredient type="meat"/>
+            {transformedIgredients}
             <BurgetIngredient type="bread-bottom"/>
         </div>
     )
