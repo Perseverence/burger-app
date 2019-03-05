f391aa75273da859825b05f3d945b5ccc24840c7
Valentin Dobrica <valy_geolgautz@yahoo.com>
2019-03-06 00:04:58 +0200

    REACT GOD

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {withRouter} from "react-router-dom";
 
 import classes from './Burger.css';
 import BurgetIngredient from './BurgerIngredient/BurgerIngredient';
@@ -29,4 +30,4 @@ const burger = (props) => {
     )
 };
 
-export default burger;
+export default withRouter(burger);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -104,30 +104,43 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
-        this.setState({loading: true});
-        // alert('You continue!');
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: "REACT GOD",
-                address: {
-                    street: 'TEST STREET 1',
-                    zipCode: '815266',
-                    country: 'Romanian Power'
-                },
-                email: 'test@yahoo.com',
-            },
-            deliveryMethod: 'fastest'
+        //     this.setState({loading: true});
+        //     // alert('You continue!');
+        //     const order = {
+        //         ingredients: this.state.ingredients,
+        //         price: this.state.totalPrice,
+        //         customer: {
+        //             name: "REACT GOD",
+        //             address: {
+        //                 street: 'TEST STREET 1',
+        //                 zipCode: '815266',
+        //                 country: 'Romanian Power'
+        //             },
+        //             email: 'test@yahoo.com',
+        //         },
+        //         deliveryMethod: 'fastest'
+        //     }
+        //
+        //     axios.post('/orders.json', order)
+        //         .then(response => {
+        //             this.setState({loading: false, purchasing: false});
+        //         })
+        //         .catch(error => {
+        //             this.setState({loading: false, purchasing: false});
+        //         });
+
+        const queryParams = [];
+
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
