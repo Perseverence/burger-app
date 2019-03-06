8bb5a9594a64cdbc74c4c4f444d5ccb1797329af
Valentin Dobrica <valentin.dobrica@rei-d-services.com>
2019-03-06 17:57:07 +0200

    REACT GOD

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,38 +103,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-
-        //     this.setState({loading: true});
-        //     // alert('You continue!');
-        //     const order = {
-        //         ingredients: this.state.ingredients,
-        //         price: this.state.totalPrice,
-        //         customer: {
-        //             name: "REACT GOD",
-        //             address: {
-        //                 street: 'TEST STREET 1',
-        //                 zipCode: '815266',
-        //                 country: 'Romanian Power'
-        //             },
-        //             email: 'test@yahoo.com',
-        //         },
-        //         deliveryMethod: 'fastest'
-        //     }
-        //
-        //     axios.post('/orders.json', order)
-        //         .then(response => {
-        //             this.setState({loading: false, purchasing: false});
-        //         })
-        //         .catch(error => {
-        //             this.setState({loading: false, purchasing: false});
-        //         });
-
         const queryParams = [];
 
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&')
 
         this.props.history.push({
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css'
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -10,20 +12,58 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+        // alert('You continue!');
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: "REACT GOD",
+                address: {
+                    street: 'TEST STREET 1',
+                    zipCode: '815266',
+                    country: 'Romanian Power'
+                },
+                email: 'test@yahoo.com',
+            },
+            deliveryMethod: 'fastest'
         }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading: false});
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            });
     }
 
     render() {
-        return (
-            <div className={classes.ContactData}>
-                <h4>Enter your Contact Data</h4>
+        let form = (
                 <form>
                     <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
                     <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
                     <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
                     <input className={classes.Input} type="text" name="postal" placeholder="Your Postal Code" />
-                    <Button btnType="Success">ORDER</Button>
+                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
+        );
+
+        if(this.state.loading) {
+            form = <Spinner/>;
+        }
+
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
             </div>
         );
     }
